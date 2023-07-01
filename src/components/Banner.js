e679385797ec12eddc1c73b0199d6c2a0a2330b3
ProfessorHusnain <mh571536@gmail.com>
2023-07-01 17:16:31 +0500

    Header component in progress

---
+++ src/components/Banner.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/Banner.css'
+
+const Banner = () => {
+  const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
+  const bannerImages = [
+    '/logo192.png',
+    '/logo512.png',
+    '/logo192.png',
+  ];
+
+  // Function to automatically change the banner every few seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentBannerIndex((prevIndex) =>
+        prevIndex === bannerImages.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // Function to manually change the banner
+  const changeBanner = (index) => {
+    setCurrentBannerIndex(index);
+  };
+
+  return (
+    <div className="banner">
+      {bannerImages.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Banner ${index}`}
+          className={currentBannerIndex === index ? 'active' : ''}
+        />
+      ))}
+      <div className="banner-controls">
+        {bannerImages.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => changeBanner(index)}
+            className={currentBannerIndex === index ? 'active' : ''}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import "../styles/Header.css";
+
+function Header(){
+
+    return (
+        <header>
+         <div className='logo'>
+          <h1>Next<span>X</span>io<span></span></h1>
+         </div>
+         <div className='menu'>
+            <h1>Menu</h1>
+         </div>
+        
+         
+        </header>  
+    );
+}
+export default Header;
