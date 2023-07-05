7d4a9b2307e3cae45de85a9f1f36f9c2b026376b
ProfessorHusnain <mh571536@gmail.com>
2023-07-05 10:07:22 +0500

    contact form added and forrter is added but not complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/Contact.css'
 
 const Contact = () => {
+ const [name,setName]=useState('')
+ const [email,setEmail]=useState('')
+ const [phone,setPhone]=useState('')
+ const [message,setMessage]=useState('')
+
+ const [formData,setFormData]=useState([])
+
+ const submitForm =(e)=>{
+  e.preventDefault();
+  
+  const entery ={name:name,email:email,phone:phone,message:message};
+  setFormData([entery,...formData])
+  
+ }
   return (
-    <div>
-      
+    <div className='contact-container' id='contact'>
+      <h1>Contact Us</h1>
+      <form className='contact' action='' onSubmit={submitForm}>
+        <div className='input-row'>
+        <label htmlFor='name'>Name</label>
+        <input type='text' id='name' name='name' value={name} onChange={(e)=>setName(e.target.value)}/>
+        </div>
+
+        <div className='input-row'>
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        </div>
+
+        <div className='input-row'>
+        <label htmlFor='phone'>Phone</label>
+        <input type='text' id='phone' name='phone' value={phone} onChange={(e)=>setPhone(e.target.value)}/>
+        </div>
+
+        <div className='input-row'>
+        <label htmlFor='message'>Message</label>
+        <textarea type='text' id='message' name='message' value={message} onChange={(e)=>setMessage(e.target.value)}/>
+        </div>
+    
+        <button type='submit'>Submit</button>
+       
+      </form>
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import '../styles/Footer.css'
 const Footer = () => {
   return (
      <footer>
-        <h4>&copy;<span>{new Date().getFullYear()}</span><span> nextXio</span></h4>
-         
+      <div className='footer-content'>
+        <h4>&copy; <span>{new Date().getFullYear()}</span><span> nextXio</span></h4>
+        <p>All Rights Reserved.</p>
+        </div>
      </footer>
   )
 }
