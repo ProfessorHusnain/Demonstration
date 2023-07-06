0456165e2c3697d2bd604df5e8d22061e255c6c7
ProfessorHusnain <mh571536@gmail.com>
2023-07-06 12:23:23 +0500

    contact design with map completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,46 +31,51 @@ const Contact = () => {
         <div className='from-container'>
 
           {/**Accutal contact form  */}
-        <form action='' onSubmit={submitForm}>
-          <div className='input-row'>
-            <label htmlFor='name'>Name</label>
-            <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} />
-          </div>
+          <form action='' onSubmit={submitForm}>
+            <div className='input-row'>
+              <label htmlFor='name'>Name</label>
+              <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
 
-          <div className='input-row'>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
+            <div className='input-row'>
+              <label htmlFor='email'>Email</label>
+              <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
 
-          <div className='input-row'>
-            <label htmlFor='phone'>Phone</label>
-            <PhoneInput
-              className='PhoneInput'
-              value={phone}
-              onChange={setPhone}
-              defaultCountry='PK'
-              international
-              countryCallingCodeEditable={false}
-              flags={flags}
-            />
-          </div>
+            <div className='input-row'>
+              <label htmlFor='phone'>Phone</label>
+              <PhoneInput
+                className='PhoneInput'
+                value={phone}
+                onChange={setPhone}
+                defaultCountry='PK'
+                international
+                countryCallingCodeEditable={false}
+                flags={flags}
+              />
+            </div>
 
-          <div className='input-row'>
-            <label htmlFor='message'>Message</label>
-            <textarea type='text' id='message' name='message' value={message} onChange={(e) => setMessage(e.target.value)} />
-          </div>
+            <div className='input-row'>
+              <label htmlFor='message'>Message</label>
+              <textarea type='text' id='message' name='message' value={message} onChange={(e) => setMessage(e.target.value)} />
+            </div>
 
-          <button type='submit'>Submit</button>
+            <button type='submit'>Submit</button>
 
-        </form>
+          </form>
         </div>
 
         {/** Map container hold the map */}
         <div className='map'>
-          <h1>Hello</h1>
-          <iframe>
+        <iframe className='map-view' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198740.1753569663!2d-77.1793870239628!3d38.89390590017632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7c6de5af6e45b%3A0xc2524522d4885d2a!2sWashington%2C%20DC%2C%20USA!5e0!3m2!1sen!2s!4v1688626026123!5m2!1sen!2s" 
+        width="600"
+         height="470" 
+         style={{border:0}} 
+         allowfullscreen=""
+          loading="lazy"
+           referrerpolicy="no-referrer-when-downgrade">
 
-          </iframe>
+           </iframe>
         </div>
       </div>
     </div>
