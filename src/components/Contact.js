fd2c550c57f5c7bf3569138e40123f65045590ab
ProfessorHusnain <mh571536@gmail.com>
2023-07-06 22:40:29 +0500

    Responsive behavior added to Price section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -75,7 +75,7 @@ const Contact = () => {
         {/** Map container hold the map */}
         <div className='map'>
         <iframe className='map-view' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198740.1753569663!2d-77.1793870239628!3d38.89390590017632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7c6de5af6e45b%3A0xc2524522d4885d2a!2sWashington%2C%20DC%2C%20USA!5e0!3m2!1sen!2s!4v1688626026123!5m2!1sen!2s" 
-        width="600"
+        width="auto"
          height="470" 
          style={{border:0}} 
          allowfullscreen=""
