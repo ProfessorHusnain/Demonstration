4c34430bec96bc7fe33a0a273dc38e2040b600a9
ProfessorHusnain <mh571536@gmail.com>
2023-07-06 10:18:57 +0500

    confirguration with contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,78 @@
 import React, { useState } from 'react'
 import '../styles/Contact.css'
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input'
+import 'react-phone-number-input/style.css'
+import flags from 'react-phone-number-input/flags'
 
 const Contact = () => {
- const [name,setName]=useState('')
- const [email,setEmail]=useState('')
- const [phone,setPhone]=useState('')
- const [message,setMessage]=useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState()
+  const [message, setMessage] = useState('')
 
- const [formData,setFormData]=useState([])
+  const [formData, setFormData] = useState([])
 
- const submitForm =(e)=>{
-  e.preventDefault();
-  
-  const entery ={name:name,email:email,phone:phone,message:message};
-  setFormData([entery,...formData])
-  
- }
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const entery = { name: name, email: email, phone: phone, message: message };
+    setFormData([entery, ...formData])
+
+  }
   return (
+    /**Container Hold the complete contact page */
     <div className='contact-container' id='contact'>
       <h1>Contact Us</h1>
-      <form className='contact' action='' onSubmit={submitForm}>
-        <div className='input-row'>
-        <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name' value={name} onChange={(e)=>setName(e.target.value)}/>
+
+      {/**Making the contact page sperated from the heading part of the page */}
+      <div className='contact'>
+
+        {/**Forms container that are align left side of the page  */}
+        <div className='from-container'>
+
+          {/**Accutal contact form  */}
+        <form action='' onSubmit={submitForm}>
+          <div className='input-row'>
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+
+          <div className='input-row'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+
+          <div className='input-row'>
+            <label htmlFor='phone'>Phone</label>
+            <PhoneInput
+              className='PhoneInput'
+              value={phone}
+              onChange={setPhone}
+              defaultCountry='PK'
+              international
+              countryCallingCodeEditable={false}
+              flags={flags}
+            />
+          </div>
+
+          <div className='input-row'>
+            <label htmlFor='message'>Message</label>
+            <textarea type='text' id='message' name='message' value={message} onChange={(e) => setMessage(e.target.value)} />
+          </div>
+
+          <button type='submit'>Submit</button>
+
+        </form>
         </div>
 
-        <div className='input-row'>
-        <label htmlFor='email'>Email</label>
-        <input type='email' id='email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        </div>
+        {/** Map container hold the map */}
+        <div className='map'>
+          <h1>Hello</h1>
+          <iframe>
 
-        <div className='input-row'>
-        <label htmlFor='phone'>Phone</label>
-        <input type='text' id='phone' name='phone' value={phone} onChange={(e)=>setPhone(e.target.value)}/>
+          </iframe>
         </div>
-
-        <div className='input-row'>
-        <label htmlFor='message'>Message</label>
-        <textarea type='text' id='message' name='message' value={message} onChange={(e)=>setMessage(e.target.value)}/>
-        </div>
-    
-        <button type='submit'>Submit</button>
-       
-      </form>
+      </div>
     </div>
   )
 }
